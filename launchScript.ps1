b95a62d832a5ec172c686e06dfa6932fcc1897ae
donnie <don.r.gearhart@gmail.com>
2019-03-20 19:59:08 -0400

    first commit of launchScript.ps1

---
+++ launchScript.ps1
@@ -0,0 +1,28 @@
+﻿#Path to Python
+$python = 'C:\iKitchen\Python\MobilConceptsOvenSafety\DeviceAPI\venv\Scripts\python.exe'
+#Path to ikitchen python file
+$ikitchen = 'C:\iKitchen\Python\MobilConceptsOvenSafety\DeviceAPI\scripts\i_kitchen.py'
+#Path to iKitchen exe
+$ikitchenApp = 'C:\iKitchen\Application\MobileConcepts.IKitchenPresenter.exe'
+#Username you want the program to run as
+$targetUser = 'ikitchen'
+
+function runScriptAsUser{
+    param(
+        $python,
+        $ikitchen,
+        $ikitchenApp,
+        $targetUser
+    )
+    if ($env:USERNAME -like $targetUser){
+        #Run the command
+        #Write-Host 'Running command: ' $command
+        #iKitchen API - to run in a command window, change WindowStyle to Minimized or comment it out completely
+        Start-Process -FilePath $python -ArgumentList $ikitchen -WindowStyle Hidden
+        #iKitchen application
+        Start-Process -FilePath $ikitchenApp
+        #If necessary this could be piped to a Wait-Process and followed by a stop-process on process to kill the API on oprocess exit
+    }
+}
+
+runScriptAsUser -python $python -ikitchen $ikitchen -ikitchenApp $ikitchenApp -targetUser $targetUser
